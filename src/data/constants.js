4ec09edc3c3851081457e7fcf2f498990f02cc14
JANAKIRAMAN <88104450+Janakiraman1021@users.noreply.github.com>
2024-01-15 15:21:45 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,245 @@
+export const Bio = {
+  name: "JanakiRaman",
+  roles: [
+    "Full Stack Developer",
+    "Data Analyst",
+    "DSA Expert",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "Im   a dynamic professional embodying the roles of a Full Stack Developer, adept at crafting end-to-end web solutions. As a skilled Data Analyst, I transforms complex datasets into actionable insights, while my prowess as a DSA Expert ensures optimized and scalable software. Simultaneously, my  creativity shines as a UI/UX Designer, delivering visually appealing and user-centric interfaces. With a strong foundation in programming, i became a versatile and adaptive contributor to the ever-evolving tech landscape.",
+  github: "https://github.com/Janakiraman1021",
+  resume:
+    "https://drive.google.com/file/d/1Agi746_P0q22KayUBwHI4mwr35VfHXRD/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/janakiraman-k-28a45a257/",
+  twitter: "https://twitter.com/janakiraman0401",
+  insta: "https://www.instagram.com/techie_jr/"   
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Front-end Developer and Trainer",
+    company: "McZeal",
+    date: "Oct 2023 - till ",
+    desc: "Works as a Frontend developer and Fullstact & Data Analyst Trainer In Mczeal from october 21 2023 ",
+    skills: [
+      "HTML",
+      "CSS",
+      "JS",
+      "React JS",
+      "Node Js",
+      "Bootstrap ",
+      "Mongo DB",
+      "Figma ",
+    ],
+
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "Jaya Engineering College, Thiruninravur",
+    date: "Oct 2022 - Sept 2026",
+    grade: "8.56 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology at Jaya Engineering College, Thiruninravur. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Information Technology",
+  },
+  {
+    id: 1,
+    school: "Bharathidasan Matriculation HIgher Secondary school , Thiruvallur ",
+    date: "Apr 2020 - Apr 2022",
+    grade: "76.5%",
+    desc: "I completed my class 12 high school education at Bharathidasan Matriculation HIgher Secondary school , Thiruvallur  where I studied Science with Computer Science.",
+    degree: "HSC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    school: "Jai Maruthi Vidhalaya Matriculation school , Thiruvallur ",
+    date: "Apr 2006 - Apr 2020",
+    grade: "65.7%",
+    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
+    degree: "SSLC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Marriage Invitations",
+    description:
+      "This Is A Project That what i done is i created a simple marriage invitaion withthe help of HTML & CSS .",
+    image:
+      "https://dev-jr.vercel.app/assets/img/invitation.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "React",
+      "JS",
+
+    ],
+    category: "web app",
+    webapp: "https://jr-wedding-invitation.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Pickup Lines Generator",
+    description:
+      "This Is a website which give you a  Rado pickup lines using HTML, CSS & JS.",
+    image:
+      "https://dev-jr.vercel.app/assets/img/pickuplines.png",
+      tags: [
+        "HTML",
+        "CSS",
+        "React",
+        "JS",
+      ],
+    category: "web app",
+    webapp: "https://janakiraman1021.github.io/PICKUP_LINES_/",
+  },
+  {
+    id: 1,
+    title: "Love Proposal website",
+    description:
+      "A simple Website For A Peoplw who is affraid to propose to their partners They can  propose to their crushes with the help of these website ",
+    image:
+      "https://dev-jr.vercel.app/assets/img/love_proposal.png",
+      tags: [
+        "HTML",
+        "CSS",
+        "React",
+        "JS",
+      ],
+    category: "web app",
+    webapp: "https://startling-truffle-372281.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Virtual Mouse",
+    description:
+      "In an era of increasingly diverse human-computer interaction methods, the Gesture-Based Control System represents a novel and intuitive approach to interface with digital devices. This project endeavors to design and implement a sophisticated system that harnesses the power of hand gestures to control computer software and hardware. Leveraging Python, OpenCV, MediaPipe, and PyAutoGUI, this system offers a seamless and responsive user experience.",
+    image:
+      "https://dev-jr.vercel.app/assets/img/virtualmouse.png",
+      tags: [
+        "Python",
+        "Open CV",
+        "Automation",
+        "AR & VR",
+      ],
+    category: "web app",
+    webapp: "https://virtual-mouse-nu.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Bank management system",
+    description:
+      "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
+    image:
+      "https://dev-jr.vercel.app/assets/img/bank.png",
+      tags: [
+        "HTML",
+        "CSS",
+        "React",
+        "JS",
+      ],
+    category: "web app",
+    github: "https://github.com/Janakiraman1021/BankProject-1.git",
+    webapp: "https://github.com/Janakiraman1021/BankProject-1.git",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2020, text: "Started my journey" },
+  { year: 2021, text: "Started my journey as a web" },
+  { year: 2023, text: "Started to work as a trainer " },
+  { year: 2023, text: "Working as a Frontend developer" },
+];
